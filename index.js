d86f886b132366bf34390038579afb9e7094de85
rfetalvero <botsot.felix@gmail.com>
2023-11-21 11:43:39 +0800

    Change server port

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use(express.json());
 
 app.use('/api/banks/', routes)
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+app.listen(9000, () => {
+    console.log(`Server Started at ${9000}`)
 })
